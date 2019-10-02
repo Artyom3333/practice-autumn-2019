d1a66d7c02bce4dbae7ca9d43bd1209f4e9e4d49
Artyom333 <artyom458787@gmail.com>
2019-10-02 14:11:51 +0300

    Первые два класса

--- src/main/java/lesson01/part1/Task02.java
+++ src/main/java/lesson01/part1/Task02.java
@@ -20,6 +20,10 @@ public class Task02 {
      */
 
     public static void main(String[] args) {
+        for(int i=0; i<3;i++){
+            System.out.println("Hello world!");
+        }
+
         // Напишите здесь ваш код
     }
 }
